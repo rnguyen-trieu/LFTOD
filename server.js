6b6bc19418230ea673452b5dd9e21f3254a58c01
Marcus Klein <marcus.klein@cgifederal.com>
2018-02-21 15:24:24 -0600

    Node adjustments

--- server.js
+++ server.js
@@ -7,6 +7,6 @@ var path = require('path');
 var express = require('express');
 
 var app = express();
-app.use('/', express.static(path.join(__dirname, '..', 'app')));
+app.use('/', express.static(path.join(__dirname, 'app')));
 app.listen(8080);
 console.log("Server is Running Press Ctrl-C to Exit");
