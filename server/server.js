cccffa1f69eb9b373328d8f169b93fe3040efea5
Marcus Klein <marcus.klein@cgifederal.com>
2018-02-21 14:51:28 -0600

    App hosted and node.js added

---
+++ server/server.js
@@ -0,0 +1,12 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+var path = require('path');
+var express = require('express');
+
+var app = express();
+app.use('/', express.static(path.join(__dirname, '..', 'app')));
+app.listen(8080);
+console.log("Server is Running Press Ctrl-C to Exit");
